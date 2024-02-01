82c01c1c186f919ee957e0153db99b922b5fd3e5
shashiamitsrivastava <92148217+shashiamitsrivastava@users.noreply.github.com>
2024-02-01 21:29:00 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,26 @@
+// Pascal Case
+// SavingAccount
+public strictfp class Demo {
+    //var g;
+    public static strictfp void main(String[] args) {
+        int ww = 100;
+        int ww2 = 0b1010101;
+        int w3 = 0123;
+        int w4 = 0x4AA;
+        long w5 = 1234_5554_1111_4444L; // Java 7 
+        char w2 = 'न';
+        System.out.println(w2);
+        float w = 10.20f;
+        // age store
+        byte age;
+        int x = 10; // Explicit Way
+        var y = 20; // Implicit Way (Java 10) 
+        // Type Inference
+        //y = true;
+        final int MAX = 100;
+        //MAX++;
+        // camelCase
+        int carSpeed = MAX; 
+
+    }
+}
---
+++ Demo2.java
@@ -0,0 +1,19 @@
+public class Demo2 {
+    public static void main(String[] args) {
+        String q = "amit";
+        q = "ram";
+        // StringBuffer , StringBuilder
+        String sql = "select * from products";
+        sql += " where price >10000";
+        sql +=" and rating>4";
+        String x = "amit"; // 1 or 0 
+        String y = "amit";
+        String z = new String("amit");
+        System.out.println(x==y);
+        System.out.println(x==z);
+        //10.equals(20)
+        System.out.println(x.equals(z));
+
+
+    }
+}
