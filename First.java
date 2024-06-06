cc12cb59ff0ab7b28e51674b59f3c497b8aba2c2
shashiamitsrivastava <92148217+shashiamitsrivastava@users.noreply.github.com>
2024-06-06 17:49:09 +0530

    Add files via upload

--- First.java
+++ First.java
@@ -1,25 +1,23 @@
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
-
-class Second{
-
-    public static void main(){
-        System.out.println("no args main");
-    }
-    public static void main(int x){
-        System.out.println("int args main");
-    }
-    static public  void main(String ...a){
-        main();
-        main(10);
-        try {
-            System.setOut(new PrintStream("/Users/amitsrivastava/Documents/dit-btech-2ndbatch-t-f/output.txt"));
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+class First{
+    static void show(int []a){
+        a[0] = 100;
+        for(int i : a){
+            System.out.println(i);
         }
-        System.out.println("Hello Java");
-       // System.out.print();
+    }
+    public static void main(String[] args) {
+        //int arr[] = new int[3]; // all are fill with 0
+        // {0,0,0}
+        //int []arr = new int[3];
+        //int [] arr = {10,20,30};
+        int [] arr = new int[]{10,20,30};
+        show(arr);
+        System.out.println("After Show Call ");
+        for(int i : arr){
+            System.out.println(i);
+        }
+
 
     }
+
 }
